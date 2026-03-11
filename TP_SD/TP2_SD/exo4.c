6fd84dfa0142367dad1188996270ec071b198679
Mathura <mathura.santhalingam@gmail.com>
2026-03-11 16:01:21 +0100

    TP2 - exo 4 et 5

--- TP_SD/TP2_SD/exo4.c
+++ TP_SD/TP2_SD/exo4.c
@@ -1,74 +1,75 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
-#include "mpi.h" //chargement de la librairie MPI
+#include "mpi.h"
 
-int main(){
-    int nb_processus, id;
+/*  mpicc -Wall exo4.c -o exo4
+    mpirun -np 2 ./exo4 => erreur
+    mpirun -np 8 ./exo4
+*/
+
+int main(int argc, char** argv) {
+    int n, id, i;
+    int *msg; 
+    int taille_msg;
+    int ack = 1; // L'accusé de réception
     MPI_Status status;
 
-    MPI_Init(NULL,NULL); // déclare un communicateur : MPI_COMM_WORLD
-    MPI_Comm_size(MPI_COMM_WORLD,&nb_processus);
-    MPI_Comm_rank(MPI_COMM_WORLD,&id);
-// acc 1 si pere, 0 pas père
-// à chacun on attend un accusé de reception
-// donc on attend autant d'acc rec qu'il y a de msg don 1er = nb_processus - 1
-// on envoie les acc de reception -> receive avec prob, regarder balise, et mettre dans tableau enfant si c'est un enfant
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &n);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-    if(id == 0) {
-        srand(time(NULL));
-        int taille_msg = (rand() % 10)+1; // taille aléatoire entre 1 et 10
-        int *message = malloc(taille_msg * sizeof(int)); // alloue la mémoire dynamiquement pour ce taille
-        printf("L'entité 0 génère un message de taille %d : [ ", taille_msg);
-        for (int k = 0; k < taille_msg; k++){
-            message[k] = rand() % 100; //on va générer des valeurs aléatoires
-            printf("%d ", message[k]);
-        }
-        printf("]\n");
-
-        // envoi initial à tout le monde sauf lui-même
-        int i;
-        for (i = 0; i < nb_processus; i++) {
-            if (i != id){
-                MPI_Send(message, taille_msg, MPI_INT, i, 0, MPI_COMM_WORLD);
-            }
-        }
-        free(message);
-        int acc_recu;
-        for (i = 1; i <nb_processus; i++){
-            MPI_Recv(&acc_recu, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-        }
-    // reception
-    } else {
-        MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status); // rec info du msg
-
-        int taille_recue;
-        MPI_Get_count(&status, MPI_INT, &taille_recue); // met taille du msg dans taille_recue
-
-        int *message_recu = malloc(taille_recue * sizeof(int)); // on alloue dynamiquement l'espace pour le message reçu
-        
-        MPI_Recv(message_recu, taille_recue, MPI_INT, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
-
-        int expediteur = status.MPI_SOURCE;
-        // affichage
-        printf("Je suis %d, j'ai reçu %d entiers de %d : [ ", id, taille_recue, status.MPI_SOURCE);
-        for(int k=0; k < taille_recue; k++){
-            printf("%d ", message_recu[k]);
-        }
-        printf("]\n");
-
-        // accusé de réception
-        int acc = 1;
-        MPI_Send(&acc, 1, MPI_INT, expediteur, 1, MPI_COMM_WORLD);
-        // inondation -> envoie à tous les voisins u différents de lui-même et différents de l'expediteur
-        for(int u = 0; u < nb_processus; u++) {
-            if (u != id && u != expediteur) {
-            }
-        }
-        printf("accusé de réception de %d\n", expediteur);
-        // Nettoyage de la mémoire
-        free(message_recu);
+    if (n < 2) {
+        if (id == 0) printf("Erreur : il faut au moins 2 processus.\n");
+        MPI_Finalize();
+        return 1;
     }
+
+    if (id == 0) {
+        srand(time(NULL));
+        taille_msg = (rand() % 10) + 1;
+        msg = (int *)malloc(taille_msg * sizeof(int));
+
+        for (i = 0; i < taille_msg; i++) {
+            msg[i] = rand() % 100;
+        }
+        // Affichage
+        printf("Processus 0 lance l'inondation ( Taille: %d, msg: ", taille_msg);
+        for (i = 0; i < taille_msg; i++) {
+            printf("%d ", msg[i]);
+        }
+        printf(")\n");
+
+        // Envoi avec la balise 0 (Message d'inondation)
+        for (i = 1; i < n; i++) {
+            MPI_Send(msg, taille_msg, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+        printf("En exécutant ce programme, on remarque que malgré l'envoi de l'accusé de réception, ");
+        printf("rien ne change fondamentalement dans l'exécution ni dans les résultats affichés ");
+        printf("car personne ne le réceptionne.");
+    } 
+    else {
+        // Réception avec la balise 0 pour ignorer les accusés de réception potentiels
+        MPI_Probe(MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+        MPI_Get_count(&status, MPI_INT, &taille_msg);
+        
+        msg = (int *)malloc(taille_msg * sizeof(int));
+
+        // On reçoit le premier message d'inondation
+        MPI_Recv(msg, taille_msg, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &status);
+        
+        // On renvoie l'entier 1 à celui qui nous a envoyé le premier message avec la balise 1
+        MPI_Send(&ack, 1, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
+        printf("Le processus %d a recu un msg de taille %d du processus %d et lui a envoyé un accusé de réceprion.\n", id, taille_msg, status.MPI_SOURCE);
+        // Relais de l'inondation aux autres (balise 0)
+        for (i = 0; i < n; i++) {
+            if (i != status.MPI_SOURCE && i != id) {
+                MPI_Send(msg, taille_msg, MPI_INT, i, 0, MPI_COMM_WORLD);
+            }
+        }
+    }
+
+    free(msg);
     MPI_Finalize();
     return 0;
-    }
+}
---
+++ TP_SD/TP2_SD/exo5.c
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "mpi.h"
+#include <unistd.h>
+
+/*  mpicc -Wall exo5.c -o exo5
+    mpirun -np 2 ./exo5 => erreur
+    mpirun -np 8 ./exo5
+
+    N.B : pour mieux voir l'inondation, j'ai utilisé "usleep(50000);" de la bibliothèque "unistd.h"
+*/
+
+int main(int argc, char **argv) {
+    // Variables
+    int n, id, i;
+    int *msg;
+    int taille_msg;
+    int acc = 1;
+    int k = 0;
+    MPI_Status status;
+
+    // Initialisation
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &n);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+    // Vérif # processus
+    if (n < 2) {
+        if (id == 0) printf("Erreur : il faut au moins 2 processus.\n");
+        MPI_Finalize();
+        return 1;
+    }
+
+    if (id == 0) {
+        // Génération d'un nb aléat de val aléatoires
+        srand(time(NULL));
+        taille_msg = (rand() % 10) + 1;
+        
+        // Allocation de mémoire
+        msg = (int *)malloc(taille_msg * sizeof(int));
+        for (i = 0; i < taille_msg; i++) {
+            msg[i] = rand() % 100;
+        }
+        
+        // Affichage des valeurs générées :
+        printf("Processus 0 lance l'inondation ( Taille: %d, msg: ", taille_msg);
+        for (i = 0; i < taille_msg; i++) {
+            printf("%d ", msg[i]);
+        }
+        printf(")\n");
+
+        // Envoi : le noeud 0 envoie à tlm donc plsrs envois donc for
+        for (i = 1; i < n; i++) {
+            MPI_Send(msg, taille_msg, MPI_INT, i, 0, MPI_COMM_WORLD);
+            usleep(50000);
+        }
+
+        // Il a envoyé n-1 msg donc il atend n-1 acc-recept
+        for (i = 0; i < n - 1; i++) {
+            MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            int count;
+            MPI_Get_count(&status, MPI_INT, &count);
+            int *temp = (int *)malloc(count * sizeof(int));
+            
+            MPI_Recv(temp, count, MPI_INT, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
+            
+            if (status.MPI_TAG == 1) {
+                k++;
+            }
+            free(temp);
+        }
+    } 
+    else {
+        // Reception de taille du msg
+        MPI_Probe(MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status); // sonde
+        MPI_Get_count(&status, MPI_INT, &taille_msg); // recup taille exacte
+        
+        // Allocation mém
+        msg = (int *)malloc(taille_msg * sizeof(int));
+        
+        // Reception du msg
+        MPI_Recv(msg, taille_msg, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &status);
+        
+        // ATTENTION : On SAUVEGARDE le parent avant que status ne soit écrasé
+        int parent = status.MPI_SOURCE;
+
+        MPI_Send(&acc, 1, MPI_INT, parent, 1, MPI_COMM_WORLD);
+
+        // Env à tous les voisins sauf expediteur et lui-m
+        for (i = 0; i < n; i++) {
+            if (i != parent && i != id) {
+                MPI_Send(msg, taille_msg, MPI_INT, i, 0, MPI_COMM_WORLD);
+            }
+        }
+        free(msg);
+
+        // Il a envoyé msg a ses n-2 voisins, donc att n-2 acc-recept
+        for (i = 0; i < n - 2; i++) {
+            MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            int count;
+            MPI_Get_count(&status, MPI_INT, &count);
+            int *temp = (int *)malloc(count * sizeof(int));
+            
+            MPI_Recv(temp, count, MPI_INT, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
+            
+            if (status.MPI_TAG == 1) {
+                k++;
+            }
+            free(temp);
+        }
+    }
+
+    // Affichage
+    if (k == 0) {
+        printf("Processus %d affiche : feuille\n", id);
+    } else {
+        printf("Processus %d affiche : parent de %d feuilles\n", id, k);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
