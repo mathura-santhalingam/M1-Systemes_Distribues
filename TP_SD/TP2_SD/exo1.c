533bb7fdc4b01de47bb39d9d09d5d128e0f1ffb3
Mathura <mathura.santhalingam@gmail.com>
2026-04-25 22:28:47 +0200

    mise à jour exo1 TP2 : recu + env = msg

--- TP_SD/TP2_SD/exo1.c
+++ TP_SD/TP2_SD/exo1.c
@@ -11,8 +11,7 @@
 
 int main(int argc, char** argv) {
     int n, id;
-    int msg_recu[2];
-    int val_a_env[2];
+    int msg[2];
     MPI_Status status;
 
     MPI_Init(&argc, &argv);
@@ -30,41 +29,37 @@ int main(int argc, char** argv) {
     if (id == 0) {
     // Création des valeurs aléatoires générées (par la racine) à envoyer
         srand(time(NULL));
-        val_a_env[0] = rand() % 100;
-        val_a_env[1] = rand() % 100;
-        printf("Processus 0 a généré le message : [%d, %d]\n", val_a_env[0], val_a_env[1]);
+        msg[0] = rand() % 100;
+        msg[1] = rand() % 100;
+        printf("Processus 0 a généré le message : [%d, %d]\n", msg[0], msg[1]);
 
         // La racine envoie à ses enfants
         int fils_gauche = 2 * id + 1;
         int fils_droit = 2 * id + 2;
 
         if (fils_gauche < n) {
-            MPI_Send(val_a_env, 2, MPI_INT, fils_gauche, 0, MPI_COMM_WORLD);
+            MPI_Send(msg, 2, MPI_INT, fils_gauche, 0, MPI_COMM_WORLD);
         }
         if (fils_droit < n) {
-            MPI_Send(val_a_env, 2, MPI_INT, fils_droit, 0, MPI_COMM_WORLD);
+            MPI_Send(msg, 2, MPI_INT, fils_droit, 0, MPI_COMM_WORLD);
         }
     } 
     // Relais
     else {
         // Reception
         int pere = (id - 1) / 2;
-        MPI_Recv(msg_recu, 2, MPI_INT, pere, 0, MPI_COMM_WORLD, &status);
-        printf("Le processus %d a recu [%d, %d] de son pere %d\n", id, msg_recu[0], msg_recu[1], pere);
+        MPI_Recv(msg, 2, MPI_INT, pere, 0, MPI_COMM_WORLD, &status);
+        printf("Le processus %d a recu [%d, %d] de son pere %d\n", id, msg[0], msg[1], pere);
         
-        // On copie dans la variable d'envoi
-        val_a_env[0] = msg_recu[0];
-        val_a_env[1] = msg_recu[1];
-
         // Relai
         int fils_gauche = 2 * id + 1;
         int fils_droit = 2 * id + 2;
 
         if (fils_gauche < n) {
-            MPI_Send(val_a_env, 2, MPI_INT, fils_gauche, 0, MPI_COMM_WORLD);
+            MPI_Send(msg, 2, MPI_INT, fils_gauche, 0, MPI_COMM_WORLD);
         }
         if (fils_droit < n) {
-            MPI_Send(val_a_env, 2, MPI_INT, fils_droit, 0, MPI_COMM_WORLD);
+            MPI_Send(msg, 2, MPI_INT, fils_droit, 0, MPI_COMM_WORLD);
         }
     }
 
@@ -111,7 +106,8 @@ int main(int argc, char** argv) {
     // ---------------------------------------------------
     if (id == 4) {
         srand(time(NULL));
-        val_a_env[0] = rand() % 100; val_a_env[1] = rand() % 100;
+        val_a_env[0] = rand() % 100;
+        val_a_env[1] = rand() % 100;
         printf("Racine 4 initie le CAST avec [%d, %d]\n", val_a_env[0], val_a_env[1]);
 
         for (int i = 0; i < nb_fils; i++) {
@@ -121,7 +117,8 @@ int main(int argc, char** argv) {
         MPI_Recv(msg_recu, 2, MPI_INT, parent, 0, MPI_COMM_WORLD, &status);
         printf("Processus %d a recu [%d, %d] de son pere %d\n", id, msg_recu[0], msg_recu[1], parent);
         
-        val_a_env[0] = msg_recu[0]; val_a_env[1] = msg_recu[1];
+        val_a_env[0] = msg_recu[0];
+        val_a_env[1] = msg_recu[1];
 
         for (int i = 0; i < nb_fils; i++) {
             MPI_Send(val_a_env, 2, MPI_INT, fils[i], 0, MPI_COMM_WORLD);
